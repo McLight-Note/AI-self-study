8f385469721f4a59d18210bd8b0845ecb376c663
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-04-04 11:46:24 +0900

    =

---
+++ 1_dars.py
@@ -0,0 +1,14 @@
+# 2025.04.04
+# Mavzu: Intro to AI
+# Muallif: Muhammadsodiq
+
+import numpy
+
+a = numpy.zeros([3,2])
+a[0,0] = 1
+a[0,1] = 2
+a[1,0] = 3
+a[1,1] = 4
+a[2,0] = 5
+a[2,1] = 6
+print(a)
