020d5887c3add0eabdeb935fac00db3785384320
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-04-11 13:51:13 +0900

    +dars

--- 1_dars.py
+++ 1_dars.py
@@ -7,9 +7,12 @@ import matplotlib.pyplot
 
 a = numpy.zeros([3,2])
 a[0,0] = 1
-a[0,1] = 2
+a[0,1] = 4
 a[1,0] = 3
-a[1,1] = 4
+a[1,1] = 3
 a[2,0] = 5
-a[2,1] = 6
-print(a)
+a[2,1] = 4
+print(a)   
+
+matplotlib.pyplot.imshow(a, interpolation='nearest')
+matplotlib.pyplot.show()
---
+++ 2_dars.py
@@ -0,0 +1,27 @@
+# 2025.04.11
+# Mavzu: Intro to AI
+# Muallif: Muhammadsodiq
+
+import numpy
+import matplotlib.pyplot
+
+class NeuralNetwork:
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        self.lr = learningrate
+        pass
+
+    def train():
+        pass
+
+    def query():
+        pass
+
+input_nodes = 3
+hidden_nodes = 3
+output_nodes = 3
+learning_rate = 0.3
+
+n = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
