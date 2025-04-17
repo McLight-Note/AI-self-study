aae3c23518746377206a219b6eec2e38bdadd75d
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-04-17 14:10:45 +0900

    +dars

--- 2_dars.py
+++ 2_dars.py
@@ -4,6 +4,7 @@
 
 import numpy
 import matplotlib.pyplot
+import scipy.special
 
 class NeuralNetwork:
     def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
@@ -11,13 +12,42 @@ class NeuralNetwork:
         self.hnodes = hiddennodes
         self.onodes = outputnodes
         self.lr = learningrate
+        
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes))
+
+        self.activation_function = lambda x: scipy.special.expit(x)
+
         pass
 
-    def train():
+    def train(self, inputs_list, targets_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+        
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
         pass
 
-    def query():
-        pass
+    def query(self, inputs_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+        return final_outputs
+        
 
 input_nodes = 3
 hidden_nodes = 3
@@ -25,3 +55,4 @@ output_nodes = 3
 learning_rate = 0.3
 
 n = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+print(numpy.random.rand(3,3))
