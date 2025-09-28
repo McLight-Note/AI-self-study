a90d9dff7367369cd329a2f7d8cd344807ca72af
Muhammadsodiq Shukirillayev <newmclight021226@gmail.com>
2025-09-28 17:47:24 +0900

    +

---
+++ Project_5_Pandas/5-Lesson.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.read_csv('pokemon.csv')
+
+healthy_pokemon = df[df['HP'] >= 200]
+print(healthy_pokemon)
+
+fast_pokemon = df[df['Speed'] >= 100]
+print(fast_pokemon)
+
+legend_pokemon = df[df['Legendary']]
+print(legend_pokemon)
+
+water_pokemon = df[(df['Type 1'] == 'Water') |
+                   (df['Type 2'] == 'Water')]
+print(water_pokemon)
+
+ff_pokemon = df[(df['Type 1'] == "Fire") &
+                (df['Type 2'] == "Flying")]
+print(ff_pokemon)
+
+# Homework
